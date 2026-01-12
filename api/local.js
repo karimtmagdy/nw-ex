a344ab57de9dce1196a4d4c32e951d792d05ea7e
Karim Tarek <126930962+karimtmagdy@users.noreply.github.com>
2026-01-12 12:57:18 +0200

    Delete api/local.js

--- api/local.js
+++
@@ -1,8 +0,0 @@
-const app = require("./server");
-const { PORT } = process.env;
-const { MiddlewareApplication } = require("./middlewares/middleware");
-
-MiddlewareApplication(app);
-app.listen(PORT || 8000, () => {
-  console.log(`Server running on port ${PORT}`);
-});
